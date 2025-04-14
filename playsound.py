8cbf2ed0cac9d2314913905ccbf1f7584f3f1296
Jordan Calhoun <165550589+nnwp-ross@users.noreply.github.com>
2025-04-14 16:12:48 -0500

    Add files via upload

---
+++ playsound.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import filedialog
+import pygame
+import random
+
+pygame.mixer.init()
+
+def play_music(filename):
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+
+def random_music():
+    track_number = random.randint(1,8)
+    play_music(f"Track0{track_number}.mp3")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Music Sample")
+
+    play_music("Track01.mp3")
+
+    root.mainloop()
