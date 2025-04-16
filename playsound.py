771524cd7dd95e55ab068d7bf018c33525b4bb2b
Alain Delgado <149970485+alexRAI10@users.noreply.github.com>
2025-04-15 23:41:07 -0500

    Update playsound.py

--- playsound.py
+++ playsound.py
@@ -5,18 +5,19 @@ import random
 
 pygame.mixer.init()
 
-def play_music(filename):
+def play_music(filename, start_at=0.0):
     pygame.mixer.music.load(filename)
-    pygame.mixer.music.play()
+    pygame.mixer.music.play(start=start_at)
 
-def random_music():
-    track_number = random.randint(1,8)
-    play_music(f"audio_tracks/Track0{track_number}.mp3")
+def random_music(start_at=0.0):
+    track_number = random.randint(1, 8)
+    filename = f"audio_tracks/Track{track_number:02}.mp3"
+    play_music(filename, start_at=start_at)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Music Sample")
 
-    play_music("audio_tracks/Track01.mp3")
+    random_music()
 
     root.mainloop()
